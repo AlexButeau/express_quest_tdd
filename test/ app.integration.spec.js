72f7a9c41101e402b1b6b08cc6c8d5d0fba3004e
AlexButeau <alexbuteau1@gmail.com>
2020-12-12 16:56:09 +0100

    added get route for one bookmark

--- test/ app.integration.spec.js
+++ test/ app.integration.spec.js
@@ -2,7 +2,12 @@ const request = require('supertest');
 
 const app = require('../app');
 
+const connection = require('../connection');
+
 describe('Test routes', () => {
+  // truncate bookmark table before each test
+  beforeEach((done) => connection.query('TRUNCATE bookmark', done));
+
   it('GET / sends "Hello World" as json', (done) => {
     request(app)
       .get('/')
@@ -18,5 +23,95 @@ describe('Test routes', () => {
 
         done();
       });
+  });
+
+  it('POST /bookmarks should fail if no url or title sent', (done) => {
+    request(app)
+      .post('/bookmarks')
+
+      .send({})
+
+      .expect(422)
+
+      .expect('Content-Type', /json/)
+
+      .then((response) => {
+        const expected = { error: 'required field(s) missing' };
+
+        expect(response.body).toEqual(expected);
+
+        done();
+      });
+  });
+
+  it('POST /bookmarks should succed if url and title sent', (done) => {
+    request(app)
+      .post('/bookmarks')
+
+      .send({ url: 'https://jestjs.io', title: 'Jest' })
+
+      .expect(201)
+
+      .expect('Content-Type', /json/)
+
+      .then((response) => {
+        const expected = {
+          id: 1,
+          url: 'https://jestjs.io',
+          title: 'Jest',
+        };
+
+        expect(response.body).toEqual(expected);
+
+        done();
+      });
+  });
+
+  describe('GET /bookmarks/:id', () => {
+    const testBookmark = { url: 'https://nodejs.org/', title: 'Node.js' };
+    beforeEach((done) =>
+      connection.query('TRUNCATE bookmark', () =>
+        connection.query('INSERT INTO bookmark SET ?', testBookmark, done)
+      )
+    );
+
+    // Write your tests HERE!
+    it('GET /bookmarks/1 should send back the bookmark for node.js.org', (done) => {
+      request(app)
+        .get('/bookmarks/1')
+
+        .expect(200)
+
+        .expect('Content-Type', /json/)
+
+        .then((response) => {
+          const expected = {
+            id: 1,
+            url: 'https://nodejs.org/',
+            title: 'Node.js',
+          };
+
+          expect(response.body).toEqual(expected);
+
+          done();
+        });
+    });
+
+    it('GET /bookmarks/2 should send back an error', (done) => {
+      request(app)
+        .get('/bookmarks/2')
+
+        .expect(404)
+
+        .expect('Content-Type', /json/)
+
+        .then((response) => {
+          const expected = { error: 'Bookmark not found' };
+
+          expect(response.body).toEqual(expected);
+
+          done();
+        });
+    });
   });
 });
